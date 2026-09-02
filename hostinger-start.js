e4358dba54c08edca91a190ec0c59adf5cff7538
guokeaaas-coder <guokeaaas@gmail.com>
2026-09-02 19:40:49 +1000

    Update hostinger-start.js

--- hostinger-start.js
+++ hostinger-start.js
@@ -1,9 +1,16 @@
 import { spawn } from "node:child_process";
 
-const app = spawn("npm", ["run", "start"], {
-  stdio: "inherit",
-  env: process.env,
-});
+const app = spawn(
+  process.execPath,
+  [
+    "./node_modules/@react-router/serve/bin.cjs",
+    "./build/server/index.js",
+  ],
+  {
+    stdio: "inherit",
+    env: process.env,
+  },
+);
 
 app.on("error", (error) => {
   console.error("Failed to start React Router server:", error);
